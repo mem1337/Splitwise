c87db82282850ede9ab44a05b536cd84d7b41eec
Alex <doommilita@gmail.com>
2024-10-24 17:56:57 +0300

    update

--- Splitwise/ExpenseManager.cs
+++ Splitwise/ExpenseManager.cs
@@ -1,13 +1,22 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Splitwise;
 
 public class ExpenseManager
 {
     public List<User> UserList;
     public List<Expense> ExpenseList;
-    public ExpenseManager(List<User> userList, List<Expense> expenseList)
+    public ExpenseManager()
     {
-        UserList = userList;
-        ExpenseList = expenseList;
+        using (StreamReader file = File.OpenText(@"json"))
+        using (JsonTextReader reader = new JsonTextReader(file))
+        {
+            JObject output = (JObject)JToken.ReadFrom(reader);
+            ExpenseManager deserializedExpenseManager = JsonConvert.DeserializeObject<ExpenseManager>(output.ToString());
+            UserList = deserializedExpenseManager.UserList;
+            ExpenseList = deserializedExpenseManager.ExpenseList;
+        }
     }
     public List<User> GetUsers()
     {
